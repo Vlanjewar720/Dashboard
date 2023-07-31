2a7c775531726286277d62ebfacb3c5c4ca16a39
Vaibhav <lanjewarvaibhav56@gmail.com>
2023-07-31 18:47:58 +0530

    first commit

---
+++ backend/db/config.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    "mongodb+srv://lanjewarvaibhav56:VaibhavLanjewar12345@cluster0.phq7pcx.mongodb.net/Edashbord?retryWrites=true&w=majority"
+  )
